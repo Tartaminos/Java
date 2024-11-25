d7b65e557785078f2ec0a57496c87113b49341e8
Johann <johannsimon@hotmail.com>
2024-11-24 21:33:43 -0300

    finish basic calc

--- Etapa 1/Calc.java
+++ Etapa 1/Calc.java
@@ -33,57 +33,116 @@ class Calc {
         switch (opcao) {
             case 1:
 
-                var numeros = recebeNumerosParaSomar(sc);
+                var numerosParaSomar = recebeNumeros(sc);
 
-                System.out.println("Resultado: " + sum(numeros));
+                System.out.println("Resultado: " + sum(numerosParaSomar));
 
                 menu();
             case 2:
-                break;
+                
+                var numerosParaSubtrair = recebeNumeros(sc);
+
+                System.out.println("Resultado: " + subtraction(numerosParaSubtrair));
+
+                menu();
             case 3:
-                break;
+
+                var numerosParaMultiplicar = recebeNumeros(sc);
+
+                System.out.println("Resultado: " + toMultiply(numerosParaMultiplicar));
+
+                menu();
             case 4:
-                break;
+
+                System.out.println("Digite o Dividendo:");
+                var dividendo = sc.nextInt();
+
+                System.out.println("Digite o Divisor: ");
+                var divisor = sc.nextInt();
+
+                System.out.println("Resultado: " + division(dividendo, divisor));
+
+                menu();
             case 0:
                 break;
             default:
-                throw new AssertionError("Opção inválida");
+                System.err.println("Opção inválida!");
+                menu();
         }
     }
+    
 
-    private static List recebeNumerosParaSomar(Scanner sc) {
+    private static List recebeNumeros(Scanner sc) {
 
-        List numeros = new ArrayList();
+        List<Integer> numeros = new ArrayList();
 
-        System.out.println("Digite quantos números deseja somar: ");
+        System.out.println("Digite a quantidade de numeros: ");
         int quantidade = sc.nextInt();
 
         for (int i = 0; i < quantidade; i++) {
             System.out.println("Digite o número inteiro: ");
             numeros.add(sc.nextInt());
+
+            System.out.println("Numeros: " + numeros);
+            
         }
 
         return numeros;
 
     }
 
-    private static String sum(List numeros){
+    private static String sum(List<Integer> numerosParaSomar) {
+
+        int resultado, resultadoSum = 0;
+
+        for (int i = 0; i < numerosParaSomar.size(); i++) {
+
+            var numero = numerosParaSomar.get(i);
+
+            resultado = resultadoSum + numero;
+
+            resultadoSum = resultado;
+        }
+
+        return String.valueOf(resultadoSum);
+
+    }
+
+    private static String subtraction(List<Integer> numerosParaSubtrair) {
 
         int resultado, resultadoFinal = 0;
 
-        for (int i = 0; i < numeros.size(); i++) {
+        for (int i = 0; i < numerosParaSubtrair.size(); i++) {
 
-            var numero = numeros.indexOf(i);
+            var numero = numerosParaSubtrair.get(i);
 
-            resultado = resultadoFinal + numero;
+            resultado = resultadoFinal - numero;
 
             resultadoFinal = resultado;
-
-            
         }
 
         return String.valueOf(resultadoFinal);
 
+    }
+
+    private static String toMultiply(List<Integer> numerosParaMultiplicar) {
+
+        int resultado = 0, resultadoMult = 1;
+
+        for (int i = 0; i < numerosParaMultiplicar.size(); i++) {
+
+            var numero = numerosParaMultiplicar.get(i);
+
+            resultado = resultadoMult * numero;
+
+            resultadoMult = resultado;
+        }
+
+        return String.valueOf(resultadoMult);
+    }
+
+    private static String division(Integer dividendo, Integer divisor) {
+        return String.valueOf(dividendo / divisor);
     }
 
 }
